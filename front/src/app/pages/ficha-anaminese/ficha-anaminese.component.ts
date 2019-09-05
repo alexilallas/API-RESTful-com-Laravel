5c253cfd7c4cc38c4011a94777ba0a11abb9cc26
Alexi Lallas Ribeiro Pereira <alexii2005@hotmail.com>
2019-09-05 00:49:12 -0300

    Formulário de Anaminese iniciado

--- front/src/app/pages/ficha-anaminese/ficha-anaminese.component.ts
+++ front/src/app/pages/ficha-anaminese/ficha-anaminese.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-declare interface TableData {
-    headerRow: string[];
-    dataRows: string[][];
-}
+import { FichaAnaminese } from './ficha-anaminese';
 
 @Component({
     selector: 'ficha-anaminese-cmp',
@@ -12,30 +8,25 @@ declare interface TableData {
 })
 
 export class FichaAnamineseComponent implements OnInit{
-    public tableData1: TableData;
-    public tableData2: TableData;
+
+    public form = new FichaAnaminese()
+
     ngOnInit(){
-        this.tableData1 = {
-            headerRow: [ 'ID', 'Name', 'Country', 'City', 'Salary'],
-            dataRows: [
-                ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
-                ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
-                ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
-                ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
-                ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
-                ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
-            ]
-        };
-        this.tableData2 = {
-            headerRow: [ 'ID', 'Name',  'Salary', 'Country', 'City' ],
-            dataRows: [
-                ['1', 'Dakota Rice','$36,738', 'Niger', 'Oud-Turnhout' ],
-                ['2', 'Minerva Hooper', '$23,789', 'Curaçao', 'Sinaai-Waas'],
-                ['3', 'Sage Rodriguez', '$56,142', 'Netherlands', 'Baileux' ],
-                ['4', 'Philip Chaney', '$38,735', 'Korea, South', 'Overland Park' ],
-                ['5', 'Doris Greene', '$63,542', 'Malawi', 'Feldkirchen in Kärnten', ],
-                ['6', 'Mason Porter', '$78,615', 'Chile', 'Gloucester' ]
-            ]
-        };
+        this.form['diabetes'] = "0"
+        this.form['hipertensao'] = "0"
+        this.form['infarto'] = "0"
+        this.form['morte_subita'] = "0"
+        this.form['cancer'] = "0"
+        this.form['fuma'] = "0"
+        this.form['alcool'] = "0"
+        this.form['hipertenso'] = "0"
+        this.form['alergico'] = "0"
+        this.form['atividade_fisica'] = "0"
+        this.form['cirurgia'] = "0"
+    }
+
+
+    onSubmit() {
+        console.log(this.form)
     }
 }
---
+++ front/src/app/pages/ficha-anaminese/ficha-anaminese.ts
@@ -0,0 +1,6 @@
+export class FichaAnaminese {
+   
+   constructor(){
+       id: Number;
+   }
+}
