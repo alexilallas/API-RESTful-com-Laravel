0c788d562aa2b5d3cce1752668096291c9fdc888
alexilrp <alexilallas.semad@saoluis.ma.gov.br>
2019-10-14 17:24:34 -0300

    Relatório em Andamento
    - Falta gerar relatório de acordo com o tipo escolhido pelo usuário no select options da tela Relatórios
    - Atualmente está gerando apenas relatório anual

--- app/Http/Controllers/System/RelatorioController.php
+++ app/Http/Controllers/System/RelatorioController.php
@@ -21,7 +21,10 @@ class RelatorioController extends Controller
 
     public function geraRelatorio()
     {
+        $tabelaExame = 'exame_fisico_geral';
+        $tabelaEvolucao = 'evolucao_pacientes';
         $criterios = $this->jsonDecode();
+        
         $meses = [
             1 => 'Janeiro',
             2 => 'Fevereiro',
@@ -37,64 +40,120 @@ class RelatorioController extends Controller
             12 => 'Dezembro'
         ];
 
-        $relatorioAnual = [];
-        $relatorioBimestral = [];
+        $relatorioAnual         = [];
+        $relatorioBimestral     = [];
+        $atendimentoEnfermagem  = [];
+        $atendimentoConsultas   = [];
 
         foreach ($criterios['ano_atendimento'] as $key => $ano) {
-            foreach ($meses as $key => $mes) {
-                // Mês
-                $frequenciaAtendimentoEnfermagem [$mes]['mes'] = $mes;
-                // Funcionário
-                $frequenciaAtendimentoEnfermagem [$mes]['funcionario'] = DB::table('exame_fisico_geral')
-                ->join('pacientes', 'exame_fisico_geral.paciente_id', '=', 'pacientes.id')
-                ->where('pacientes.tipo', 'Funcionário')
-                ->whereRaw("DATE_FORMAT(data, '%m') = ".$key)
-                ->whereRaw("DATE_FORMAT(data, '%Y') = ".$ano)
-                ->count();
-                // Aluno
-                $frequenciaAtendimentoEnfermagem [$mes]['aluno'] = DB::table('exame_fisico_geral')
-                ->join('pacientes', 'exame_fisico_geral.paciente_id', '=', 'pacientes.id')
-                ->where('pacientes.tipo', 'Aluno')
-                ->whereRaw("DATE_FORMAT(data, '%m') = ".$key)
-                ->whereRaw("DATE_FORMAT(data, '%Y') = ".$ano)
-                ->count();
-                // Dependente
-                $frequenciaAtendimentoEnfermagem [$mes]['dependente'] = DB::table('exame_fisico_geral')
-                ->join('pacientes', 'exame_fisico_geral.paciente_id', '=', 'pacientes.id')
-                ->where('pacientes.tipo', 'Dependente')
-                ->whereRaw("DATE_FORMAT(data, '%m') = ".$key)
-                ->whereRaw("DATE_FORMAT(data, '%Y') = ".$ano)
-                ->count();
-                // Comunidade
-                $frequenciaAtendimentoEnfermagem [$mes]['comunidade'] = DB::table('exame_fisico_geral')
-                ->join('pacientes', 'exame_fisico_geral.paciente_id', '=', 'pacientes.id')
-                ->where('pacientes.tipo', 'Comunidade')
-                ->whereRaw("DATE_FORMAT(data, '%m') = ".$key)
-                ->whereRaw("DATE_FORMAT(data, '%Y') = ".$ano)
-                ->count();
-                // Serviço Prestado
-                $frequenciaAtendimentoEnfermagem [$mes]['servico_prestado'] = DB::table('exame_fisico_geral')
-                ->join('pacientes', 'exame_fisico_geral.paciente_id', '=', 'pacientes.id')
-                ->where('pacientes.tipo', 'Serviço Prestado')
-                ->whereRaw("DATE_FORMAT(data, '%m') = ".$key)
-                ->whereRaw("DATE_FORMAT(data, '%Y') = ".$ano)
-                ->count();
-                // Soma
-                $frequenciaAtendimentoEnfermagem [$mes]['soma'] =
-                $frequenciaAtendimentoEnfermagem [$mes]['funcionario']
-                + $frequenciaAtendimentoEnfermagem [$mes]['aluno']
-                + $frequenciaAtendimentoEnfermagem [$mes]['dependente']
-                + $frequenciaAtendimentoEnfermagem [$mes]['comunidade']
-                + $frequenciaAtendimentoEnfermagem [$mes]['servico_prestado'];
+            foreach ($meses as $numMes => $mes) {
+                foreach ($criterios['tipo_atendimento'] as $atendimento) {
+                    if ($atendimento == 'Enfermagem') {
+                        $atendimentoEnfermagem [$mes] ['mes'] = $mes;
+                        $atendimentoEnfermagem [$mes] = $this->getAtendimentoData($ano, $numMes, $tabelaExame);
+                    }
 
+                    if ($atendimento == 'Consulta') {
+                        $atendimentoConsultas [$mes]['mes'] = $mes;
+                        $atendimentoConsultas [$mes] = $this->getAtendimentoData($ano, $numMes, $tabelaEvolucao);
+                    }
+                }
+            }
 
-                $frequenciaAtendimentoConsultas [$mes] = DB::table('evolucao_pacientes')
-                ->whereRaw("DATE_FORMAT(data, '%m') = ".$key)
-                ->whereRaw("DATE_FORMAT(data, '%Y') = ".$ano)
-                ->count();
+            if (isset($atendimentoEnfermagem)) {
+                $relatorioAnual [] = $atendimentoEnfermagem;
+            }
+            if (isset($atendimentoConsultas)) {
+                $relatorioAnual [] = $atendimentoConsultas;
+            }
+        }
+        
+        $relatorioAnual = $this->trabalhaRelatorioAnual($relatorioAnual, $meses);
+        
+        return $this->jsonSuccess('Gráficos', compact(['relatorioAnual']));
+    }
+
+    /**
+     * Retorna a quantidade de atendimentos de algum mês em algum ano
+     *
+     * @param int $ano O ano
+     * @param int $numMes O número do mês
+     * @param string $tabela A tabela que será buscada as informações
+     *
+     * @return array O array com todas os quantitativos de algum mês em algum ano
+     */
+    public function getAtendimentoData($ano, $numMes, $tabela)
+    {
+        // Tipos de atendimento
+        $atendimentos ['funcionario']      = $this->getLineTableCount($tabela, $ano, $numMes, 'Funcionário');
+        $atendimentos ['aluno']            = $this->getLineTableCount($tabela, $ano, $numMes, 'Aluno');
+        $atendimentos ['dependente']       = $this->getLineTableCount($tabela, $ano, $numMes, 'Dependente');
+        $atendimentos ['comunidade']       = $this->getLineTableCount($tabela, $ano, $numMes, 'Comunidade');
+        $atendimentos ['servico_prestado'] = $this->getLineTableCount($tabela, $ano, $numMes, 'Serviço Prestado');
+
+        // Soma
+        $atendimentos ['soma'] =
+          $atendimentos ['funcionario']
+        + $atendimentos ['aluno']
+        + $atendimentos ['dependente']
+        + $atendimentos ['comunidade']
+        + $atendimentos ['servico_prestado'];
+
+        return $atendimentos;
+    }
+    
+    /**
+     * Retorna a quantidade de linha em alguma tabela com base no mês e ano e critério
+     *
+     * @param int $ano O ano
+     * @param int $mes O mês
+     * @param string $criterio o tipo de paciente
+     *
+     * @return array A quantidade de linhas
+     */
+    public function getLineTableCount($tabela, $ano, $mes, $criterio)
+    {
+        return DB::table($tabela)
+            ->join('pacientes', $tabela.'.paciente_id', '=', 'pacientes.id')
+            ->where('pacientes.tipo', $criterio)
+            ->whereRaw("DATE_FORMAT(data, '%m') = ".$mes)
+            ->whereRaw("DATE_FORMAT(data, '%Y') = ".$ano)
+            ->count();
+    }
+
+    /**
+     * Trata os dados que serão transformados no relatório anual
+     *
+     * @param array $relatorioAnual Os dados brutos do relatório
+     * @param array $meses Todos os meses do ano
+     *
+     * @return array O relatório com os dados organizados e prontos para
+     * serem enviados à tela de relatório
+     */
+    public function trabalhaRelatorioAnual($relatorioAnual, $meses)
+    {
+        // Gera um array igual ao $relatorioAnual com valores zerados para o calculo a seguir
+        foreach ($meses as $numMes => $mes) {
+            $relatorioAnualTrabalhado[$mes]['funcionario']      = 0;
+            $relatorioAnualTrabalhado[$mes]['aluno']            = 0;
+            $relatorioAnualTrabalhado[$mes]['dependente']       = 0;
+            $relatorioAnualTrabalhado[$mes]['comunidade']       = 0;
+            $relatorioAnualTrabalhado[$mes]['servico_prestado'] = 0;
+            $relatorioAnualTrabalhado[$mes]['soma']             = 0;
+        }
+        // Soma o quantitativo dos meses de todos os anos
+        foreach ($relatorioAnual as $key => $relatorio) {
+            foreach ($relatorio as $mes => $dados) {
+                $relatorioAnualTrabalhado[$mes]['mes']               = $mes;
+                $relatorioAnualTrabalhado[$mes]['funcionario']      += $dados['funcionario'];
+                $relatorioAnualTrabalhado[$mes]['aluno']            += $dados['aluno'];
+                $relatorioAnualTrabalhado[$mes]['dependente']       += $dados['dependente'];
+                $relatorioAnualTrabalhado[$mes]['comunidade']       += $dados['comunidade'];
+                $relatorioAnualTrabalhado[$mes]['servico_prestado'] += $dados['servico_prestado'];
+                $relatorioAnualTrabalhado[$mes]['soma']             += $dados['soma'];
             }
         }
 
-        return $this->jsonSuccess('Gráficos', compact(['frequenciaAtendimentoEnfermagem']));
+        return $relatorioAnualTrabalhado;
     }
 }
--- front/src/app/pages/relatorios/relatorios.component.ts
+++ front/src/app/pages/relatorios/relatorios.component.ts
@@ -12,11 +12,14 @@ export class RelatorioComponent implements OnInit {
 
   public _tipo_atendimento: any;
   public _ano_atendimento: any;
+  public _tipo_relatorio: any;
 
   public form: any = new Relatorio();
   public relatorioAnual: any = [];
+  public showRelatorioAnual: boolean = false;
   public totalAnual: number;
   public relatorioBimestral: any = [];
+  public showRelatorioBimestral: boolean = false;
   public totalBimestral: number;
 
   constructor(
@@ -27,6 +30,7 @@ export class RelatorioComponent implements OnInit {
   ngOnInit() {
     this.getBase()
     this._tipo_atendimento = ['Consulta', 'Enfermagem']
+    this._tipo_relatorio = ['Bimestral', 'Trimestral', 'Semestral', 'Anual']
   }
 
   getBase() {
@@ -37,6 +41,37 @@ export class RelatorioComponent implements OnInit {
   }
 
   geraRelatorio() {
+
+    this.showRelatorios()
+    this.removeEmpty()
+
+    if (this.form.tipo_atendimento && this.form.ano_atendimento) {
+      this.relatorioService.getRelatorioData(this.form)
+        .subscribe(response => {
+          this.relatorioAnual = Object.values(response.data.relatorioAnual)
+          this.relatorioBimestral = response.relatorioBimestral
+          this.totalAnual = this.relatorioAnual.reduce((currentTotal, item) => {
+            return item.soma + currentTotal
+          }, 0)
+        })
+    }
+  }
+
+  showRelatorios() {
+    if (this.form.tipo_relatorio.find(relatorio => { return relatorio == 'Anual' })) {
+      this.showRelatorioAnual = true
+    } else {
+      this.showRelatorioAnual = false
+    }
+
+    if (this.form.tipo_relatorio.find(relatorio => { return relatorio == 'Bimestral' })) {
+      this.showRelatorioBimestral = true
+    } else {
+      this.showRelatorioBimestral = false
+    }
+  }
+
+  removeEmpty() {
     if (this.form.tipo_atendimento) {
       if (this.form.tipo_atendimento.length == 0) {
         delete this.form.tipo_atendimento
@@ -46,17 +81,6 @@ export class RelatorioComponent implements OnInit {
       if (this.form.ano_atendimento.length == 0) {
         delete this.form.ano_atendimento
       }
-    }
-
-    if (this.form.tipo_atendimento && this.form.ano_atendimento) {
-      this.relatorioService.getRelatorioData(this.form)
-        .subscribe(response => {
-          this.relatorioAnual = Object.values(response.data.frequenciaAtendimentoEnfermagem)
-          this.relatorioBimestral = response.relatorioBimestral
-          this.totalAnual = this.relatorioAnual.reduce((currentTotal, item) =>{
-            return item.soma + currentTotal
-          },0)
-        })
     }
   }
 
